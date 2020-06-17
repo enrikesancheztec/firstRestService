0af67fa0001ec51719fda52fe134960565d14fda
Enrique Sanchez <enrike.sanchez@gmail.com>
2020-06-16 21:30:32 -0600

    first commit

---
+++ src/main/java/mx/tec/lab/FirstRestServiceApplication.java
@@ -0,0 +1,13 @@
+package mx.tec.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestServiceApplication.class, args);
+	}
+
+}
